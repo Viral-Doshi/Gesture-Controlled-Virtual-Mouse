ebeeb01ab264f8b6d06dc03629d6216f03f21769
ankit-4129 <ankitsharma.rbt2@gmail.com>
2021-08-08 12:31:38 +0530

    Update Gesture_Controller.py

--- Media_Pipe/Gesture_Controller.py
+++ Media_Pipe/Gesture_Controller.py
@@ -27,50 +27,66 @@ class Gest(IntEnum):
     LAST4 = 15
     PALM = 31
     FIRST2 = 12
+
     V_GEST = 33
     TWO_FINGER_CLOSED = 34
-    PINCH = 35
+    PINCH_MAJOR = 35
+    PINCH_MINOR = 36
+
+class HLabel(IntEnum):
+    MINOR = 0
+    MAJOR = 1
 
 
 class Hand_Recog:
-    finger = 0
-    ori_gesture = Gest.PALM
-    prev_gesture = Gest.PALM
-    frame_count = 0
     
-    def get_signed_dist(point):
+    def __init__(self, hand_label):
+        self.finger = 0
+        self.ori_gesture = Gest.PALM
+        self.prev_gesture = Gest.PALM
+        self.frame_count = 0
+        self.hand_result = None
+        self.hand_label = hand_label
+    
+    def set_hand_result(self, hand_result):
+        self.hand_result = hand_result
+
+    def get_signed_dist(point, hand_result):
         sign = -1
-        if Gest_Ctrl.hand_result.landmark[point[0]].y < Gest_Ctrl.hand_result.landmark[point[1]].y:
+        if hand_result.landmark[point[0]].y < hand_result.landmark[point[1]].y:
             sign = 1
-        dist = (Gest_Ctrl.hand_result.landmark[point[0]].x - Gest_Ctrl.hand_result.landmark[point[1]].x)**2
-        dist += (Gest_Ctrl.hand_result.landmark[point[0]].y - Gest_Ctrl.hand_result.landmark[point[1]].y)**2
+        dist = (hand_result.landmark[point[0]].x - hand_result.landmark[point[1]].x)**2
+        dist += (hand_result.landmark[point[0]].y - hand_result.landmark[point[1]].y)**2
         dist = math.sqrt(dist)
         return dist*sign
     
-    def get_dist(point):
-        dist = (Gest_Ctrl.hand_result.landmark[point[0]].x - Gest_Ctrl.hand_result.landmark[point[1]].x)**2
-        dist += (Gest_Ctrl.hand_result.landmark[point[0]].y - Gest_Ctrl.hand_result.landmark[point[1]].y)**2
+    def get_dist(point, hand_result):
+        dist = (hand_result.landmark[point[0]].x - hand_result.landmark[point[1]].x)**2
+        dist += (hand_result.landmark[point[0]].y - hand_result.landmark[point[1]].y)**2
         dist = math.sqrt(dist)
         return dist
     
-    def get_mxdist(point): #manhaten x distance
-        return Gest_Ctrl.hand_result.landmark[point[0]].x - Gest_Ctrl.hand_result.landmark[point[1]].x
+    #def get_mxdist(point, hand_result): #manhaten x distance
+    #    return hand_result.landmark[point[0]].x - hand_result.landmark[point[1]].x
 
-    def render_finger_state(frame):
+    def render_finger_state(self, frame):
+        if self.hand_result == None:
+            return frame
+
         points = [[8,5,0],[12,9,0],[16,13,0],[20,17,0]]
         label = ["dist_ratio :"]
-        Hand_Recog.finger = 0
-        Hand_Recog.finger = Hand_Recog.finger | 0 #thumb
+        self.finger = 0
+        self.finger = self.finger | 0 #thumb
         for idx,point in enumerate(points):
             
-            dist = Hand_Recog.get_signed_dist(point[:2])
-            dist2 = Hand_Recog.get_signed_dist(point[1:])
+            dist = Hand_Recog.get_signed_dist(point[:2],self.hand_result)
+            dist2 = Hand_Recog.get_signed_dist(point[1:],self.hand_result)
             
             try:
                 ratio = round(dist/dist2,1)
-                Hand_Recog.finger = Hand_Recog.finger << 1
+                self.finger = self.finger << 1
                 if ratio > 0.5 :
-                    Hand_Recog.finger = Hand_Recog.finger | 1
+                    self.finger = self.finger | 1
                 label[0] += str(ratio) + ','
             except:
                 label[0] += "Division by 0"
@@ -80,12 +96,21 @@ class Hand_Recog:
         
         return frame
     
-    def get_gesture():
+    def get_hand():
+        pass
+    
+    def get_gesture(self):
+        if self.hand_result == None:
+            return Gest.PALM
+
         current_gesture = Gest.PALM
-        if Hand_Recog.finger in [Gest.LAST3,Gest.LAST4] and Hand_Recog.get_dist([8,4]) < 0.05:
-            current_gesture = Gest.PINCH
+        if self.finger in [Gest.LAST3,Gest.LAST4] and Hand_Recog.get_dist([8,4],self.hand_result) < 0.05:
+            if self.hand_label == HLabel.MINOR :
+                current_gesture = Gest.PINCH_MINOR
+            else:
+                current_gesture = Gest.PINCH_MAJOR
             #print(Hand_Recog.get_dist([8,4]))
-        elif Gest.FIRST2 == Hand_Recog.finger :
+        elif Gest.FIRST2 == self.finger :
             point = [[8,12],[5,9]]
             dist1 = Hand_Recog.get_dist(point[0])
             dist2 = Hand_Recog.get_dist(point[1])
@@ -103,18 +128,18 @@ class Hand_Recog:
                     current_gesture =  Gest.MID
             
         else:
-            current_gesture =  Hand_Recog.finger
+            current_gesture =  self.finger
         
-        if current_gesture == Hand_Recog.prev_gesture:
-            Hand_Recog.frame_count += 1
+        if current_gesture == self.prev_gesture:
+            self.frame_count += 1
         else:
-            Hand_Recog.frame_count = 0
+            self.frame_count = 0
 
-        Hand_Recog.prev_gesture = current_gesture
+        self.prev_gesture = current_gesture
 
-        if Hand_Recog.frame_count > 4 :
-            Hand_Recog.ori_gesture = current_gesture
-        return Hand_Recog.ori_gesture
+        if self.frame_count > 4 :
+            self.ori_gesture = current_gesture
+        return self.ori_gesture
 
 class Mouse:
     tx_old = 0
@@ -165,12 +190,12 @@ class Mouse:
         volume.SetMasterVolumeLevelScalar(currentVolumeLv, None)
         #print("changed vol",volume.GetMasterVolumeLevelScalar())
     
-    def get_dz(point):
-        return abs(Gest_Ctrl.hand_result.landmark[point[0]].z - Gest_Ctrl.hand_result.landmark[point[1]].z)
+    #def get_dz(point):
+    #    return abs(Gest_Ctrl.hand_result.landmark[point[0]].z - Gest_Ctrl.hand_result.landmark[point[1]].z)
     
-    def get_position():
+    def get_position(hand_result):
         point = 9
-        position = [Gest_Ctrl.hand_result.landmark[point].x ,Gest_Ctrl.hand_result.landmark[point].y]
+        position = [hand_result.landmark[point].x ,hand_result.landmark[point].y]
         sx,sy = pyautogui.size()
         x_old,y_old = pyautogui.position()
         x = int(position[0]*sx)
@@ -202,15 +227,15 @@ class Mouse:
         #print("r " , ratio)
         return (x,y)
 
-    def move_mouse(gesture):
+    def handle_controls(gesture, hand_result):
         
-        x,y = Mouse.get_position()
+        x,y = Mouse.get_position(hand_result)
         
         #flag reset
         if gesture != Gest.FIST and Mouse.grabflag:
             Mouse.grabflag = False
             pyautogui.mouseUp(button = "left")
-        if gesture != Gest.PINCH and Mouse.pinchstartycoord is not None:
+        if gesture != Gest.PINCH_MAJOR and Mouse.pinchstartycoord is not None:
             print("pinch lv STOP " , Mouse.pinchlv)
             Mouse.pinchstartycoord = None
         
@@ -239,7 +264,9 @@ class Mouse:
             Mouse.flag = False
         elif gesture == Gest.PINKY:
             print('pinky ksndkskk')
-        elif gesture == Gest.PINCH:
+        elif gesture == Gest.PINCH_MINOR:
+            print('PINCH MINOR')
+        elif gesture == Gest.PINCH_MAJOR:
             if Mouse.pinchstartycoord is None:
                 Mouse.pinchstartxcoord = Gest_Ctrl.hand_result.landmark[8].x
                 Mouse.pinchstartycoord = Gest_Ctrl.hand_result.landmark[8].y
@@ -285,7 +312,9 @@ class Gest_Ctrl:
     cap = None
     CAM_HEIGHT = None
     CAM_WIDTH = None
-    hand_result = None
+    hr_major = None #right
+    hr_minor = None #left
+    dom_hand = True # right is dom
 
     def __init__(self):
         Gest_Ctrl.gc_mode = 1
@@ -294,18 +323,43 @@ class Gest_Ctrl:
         Gest_Ctrl.CAM_WIDTH = Gest_Ctrl.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     
     def calculate_position(self):
-        final_x = (Gest_Ctrl.hand_result.landmark[8].x +  Gest_Ctrl.hand_result.landmark[12].x)/2
-        final_y = (Gest_Ctrl.hand_result.landmark[8].y +  Gest_Ctrl.hand_result.landmark[12].y)/2
+        final_x = (Gest_Ctrl.hr_major.landmark[8].x +  Gest_Ctrl.hr_major.landmark[12].x)/2
+        final_y = (Gest_Ctrl.hr_major.landmark[8].y +  Gest_Ctrl.hr_major.landmark[12].y)/2
         return [final_x,final_y]
     
-    def ShowLocation(self):
+    def classify_hands(results):
+        left , right = None,None
         try:
-            print(Gest_Ctrl.hand_result.landmark[12].z)
+            handedness_dict = MessageToDict(results.multi_handedness[0])
+            if handedness_dict['classification'][0]['label'] == 'Right':
+                right = results.multi_hand_landmarks[0]
+            else :
+                left = results.multi_hand_landmarks[0]
         except:
-            print('Hi')
-    
+            pass
+
+        try:
+            handedness_dict = MessageToDict(results.multi_handedness[1])
+            if handedness_dict['classification'][0]['label'] == 'Right':
+                right = results.multi_hand_landmarks[1]
+            else :
+                left = results.multi_hand_landmarks[1]
+        except:
+            pass
+        
+        if Gest_Ctrl.dom_hand == True:
+            Gest_Ctrl.hr_major = right
+            Gest_Ctrl.hr_minor = left
+        else :
+            Gest_Ctrl.hr_major = left
+            Gest_Ctrl.hr_minor = right
+
     def start(self):
-        with mp_hands.Hands(max_num_hands = 1,min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
+
+        handmajor = Hand_Recog(HLabel.MAJOR)
+        handminor = Hand_Recog(HLabel.MINOR)
+
+        with mp_hands.Hands(max_num_hands = 2,min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
             while Gest_Ctrl.cap.isOpened() and Gest_Ctrl.gc_mode:
                 success, image = Gest_Ctrl.cap.read()
                 if not success:
@@ -315,28 +369,29 @@ class Gest_Ctrl:
                 image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
                 image.flags.writeable = False
                 results = hands.process(image)
-                '''
-                try:
-                    
-                    handedness_dict = MessageToDict(results.multi_handedness[0])
-                    print(handedness_dict['classification'][0]['label'])
-                except:
-                    print("No hand Label")
-                try:
-                    handedness_dict = MessageToDict(results.multi_handedness[1])
-                    print(handedness_dict['classification'][0]['label'])
-                except:
-                    print("No hand Label")
-                '''
+                
                 image.flags.writeable = True
                 image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
                 if results.multi_hand_landmarks:
-                    Gest_Ctrl.hand_result = results.multi_hand_landmarks[0]
-                    pos = self.calculate_position()
+                    
+                    Gest_Ctrl.classify_hands(results)
+                    #pos = self.calculate_position()
+                    handmajor.set_hand_result(Gest_Ctrl.hr_major)
+                    handminor.set_hand_result(Gest_Ctrl.hr_minor)
+
                     ##hand
-                    image = Hand_Recog.render_finger_state(image)
-                    gest_name = Hand_Recog.get_gesture()
-                    Mouse.move_mouse(gest_name)
+                    image = handmajor.render_finger_state(image)
+                    image = handminor.render_finger_state(image)
+                    gest_name = handminor.get_gesture()
+
+                    if gest_name == Gest.PINCH_MINOR:
+                        Mouse.handle_controls(gest_name, handminor.hand_result)
+                    else:
+                        gest_name = handmajor.get_gesture()
+                        Mouse.handle_controls(gest_name, handmajor.hand_result)
+                    #image = Hand_Recog.render_finger_state(image)
+                    #gest_name = Hand_Recog.get_gesture()
+                    
                     for hand_landmarks in results.multi_hand_landmarks:
                         mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
                 else:
