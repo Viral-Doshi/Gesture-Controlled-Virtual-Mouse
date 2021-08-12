f6b5fa2510720e6cd4489c7dabb16a88e17a84a9
Viral Doshi <viraldoshi321@gmail.com>
2021-08-12 12:11:22 +0530

    Delete custom.js

--- Media_Pipe/web/js/custom.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$('.chat_icon').click(function() {
-		$('.chat_box').toggleClass('active');
-	});
-
-	$('.my-conv-form-wrapper').convform({selectInputStyle: 'disable'})
-});
